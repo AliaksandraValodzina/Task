3fff7c8e9f3dbaef33132bffe96c8e8581a815e7
Aliaksandra Valodzina <shoora.volodina@gmail.com>
2016-04-18 17:59:02 +0300

    tasks a-i

---
+++ MyTasks/ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int  Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Devide(int a, int b)
+        {
+            int dev = a / b;
+            return dev;
+
+        }
+
+
+    }
+}

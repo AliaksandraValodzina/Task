312853c1bb2af60ec2a8d1e50f9a447ce686dbaf
Aliaksandra Valodzina <shoora.volodina@gmail.com>
2016-04-18 16:39:47 +0300

    a - i tasks

--- MyTasks/ClassLibrary1/Class1.cs
+++ MyTasks/ClassLibrary1/Class1.cs
@@ -8,5 +8,29 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int  Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Devide(int a, int b)
+        {
+            int dev = a / b;
+            return dev;
+
+        }
+
+
     }
 }
--- MyTasks/ConsoleApplication1/Program.cs
+++ MyTasks/ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1;
 
 namespace ConsoleApplication1
 {
@@ -10,31 +11,55 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int a = 0;
+            int b = 0;
             try
             {
-                do
-                {
-                    Console.WriteLine("Введите два числа в формате:" );
-                    Console.WriteLine("[первое число] [пробел] [второе число]");
-                    Console.WriteLine("Пример: 12 32");
-                    //Read line, and split it by whitespace into an array of strings
-                    string[] tokens = Console.ReadLine().Split();
+                String readFrom = System.Configuration.ConfigurationManager.AppSettings["read"];
+                if (readFrom.Equals("resource")){
+                    String aStr = Resource1.num1;
+                    a = Int32.Parse(aStr);
 
-                    //Parse element 0
-                    int a = int.Parse(tokens[0]);
+                    String bStr = Resource1.num2;
+                    b = Int32.Parse(bStr);
 
-                    //Parse element 1
-                    int b = int.Parse(tokens[1]);
+                } else if (readFrom.Equals("console")) { 
+                Console.WriteLine("Введите два числа в формате:");
+                Console.WriteLine("[первое число] [пробел] [второе число]");
+                Console.WriteLine("Пример: 12 32");
+                //Read line, and split it by whitespace into an array of strings
+                string[] tokens = Console.ReadLine().Split();
 
+                //Parse element 0
+                a = int.Parse(tokens[0]);
 
-                    Console.WriteLine("Сумма чисел {0} и {1} равна {2}", a, b, a + b);
-
-                    Console.WriteLine("Для выхода нажмите Escape; для продолжения - любую другую клавишу");
-                }
-                while (Console.ReadKey().Key != ConsoleKey.Escape);
+                //Parse element 1
+                b = int.Parse(tokens[1]);
             }
-            catch (Exception ex)
-            {
+
+                    String conf = System.Configuration.ConfigurationManager.AppSettings["choice"];
+
+                    if (conf.Equals("library")) {
+                        Class1 lib = new Class1();
+                        int sum = lib.Sum(a, b);
+                        Console.WriteLine("Сумма чисел {0} и {1} равна {2}", a, b, sum);
+
+                        int sub = lib.Sub(a, b);
+                        Console.WriteLine("Разность чисел {0} и {1} равна {2}", a, b, sub);
+
+                        int mult = lib.Mult(a, b);
+                        Console.WriteLine("произведение чисел {0} и {1} равно {2}", a, b, mult);
+
+                        int dev = lib.Devide(a, b);
+                        Console.WriteLine("Деление чисел {0} и {1} равно {2}", a, b, dev);
+                        Console.ReadLine();
+
+                } else if (conf.Equals("console")) {
+                        Console.WriteLine("Сумма чисел {0} и {1} равна {2}", a, b, a + b);
+                        Console.ReadLine();
+                }
+
+            } catch (Exception ex){
                 Console.WriteLine(ex.Message);
                 Console.ReadLine();
             }

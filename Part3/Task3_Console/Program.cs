7e5a9f3430ec50346947b9c4c063cccdfaf80c15
Aliaksandra Valodzina <shoora.volodina@gmail.com>
2016-04-21 13:04:52 +0300

    task three

--- Part3/Task3_Console/Program.cs
+++ Part3/Task3_Console/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
+using Equation;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuadraticDecision
 {
@@ -11,8 +9,37 @@ namespace QuadraticDecision
         static void Main(string[] args)
         {
             // Choose a type of equation
+            Console.Write("Plese enter Q if you want to solve a quadratic equation\r\n");
+            Console.Write("or enter L if you want to solve a linear equation.\r\n");
+            string typeEquation = Console.ReadLine();
+            Quadratic quadratic = new Quadratic();
 
             // Input value A, B, C
+            switch (typeEquation)
+            {
+                // Solve a quadratic equation.
+                case "Q":
+                    // Enter A, B, C
+                    Console.Write("Plese enter constants of a quadratic equation: A B C\r\n");
+                    List<int> constants = quadratic.InputValue();
+                    // Find roots
+                    quadratic.OuadraticDecision(constants);
+                    break;
+
+                // Solve a linear equation.
+                case "L":
+                    // Enter A, B
+                    Console.Write("Plese enter constants of a linear equation: A B\r\n");
+                    List<int> constantsLinear = quadratic.InputValue();
+                    // Find root
+                    quadratic.LinearDecision(constantsLinear);
+                    break;
+
+                default:
+                    String message = $"Default input type: {typeEquation}";
+                    Console.WriteLine(message);
+                    break;
+            }
+        }
         }
     }
-}
--- Part3/Task3_Library/Class1.cs
+++ Part3/Task3_Library/Class1.cs
@@ -1,23 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace QuadraticEquation
+
+namespace Equation
 {
     public class Quadratic
     {
-        public void OuadraticDecision() {
+        // Find roots of quadratic eqution
+        public void OuadraticDecision(List<int> constants)
+        {
+            int a = constants[0];
+            int b = constants[1];
+            int c = constants[2];
 
+            double d = b * b - 4 * a * c;
+
+            if (d >= 0)
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+                Console.WriteLine("The roots of your equation are: x1 = {0:0.##}; x2 = {1:0.##}.", x1, x2);
+            } else {
+                Console.WriteLine("Equation does not have roots.");
+            }
+            Console.ReadLine();
         }
 
-        public int InputValue() {
-            string valueStr = Console.ReadLine();
-            int valueInt = Int32.Parse(valueStr);
-            return valueInt;
+        // Find roots of linear eqution
+        public void LinearDecision(List<int> constants)
+        {
+            int a = constants[0];
+            int b = constants[1];
+
+            double x = b / a;
+
+            Console.WriteLine("The root of your equation are: x = {0:0.##}.", x);
+
+            Console.ReadLine();
         }
 
+        // Read A, B, C values
+        public List<int> InputValue()
+        {
+            string constRead = Console.ReadLine();
+            string[] constArray = constRead.Split(' ');
+            List<int> constants = new List<int>();
+            int oneConst;
+            foreach (string s in constArray)
+            {
+                if (Int32.TryParse(s, out oneConst))
+                {
+                    constants.Add(oneConst);
+                }
+            }
+            return constants;
+        }
 
+        // Write to log file
+        public void Logger()
+        {
+            
+        }
     }
 }

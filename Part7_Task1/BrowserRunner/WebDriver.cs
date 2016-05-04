7080a28b3f3558ddc535bd6f61cb7f9f09fcbd4f
Aliaksandra Valodzina <shoora.volodina@gmail.com>
2016-05-04 17:52:25 +0300

    task

---
+++ Part7_Task1/BrowserRunner/WebDriver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace BrowserRunner
+{
+    class WebDriver : IWebDriver
+    {
+        private static InitWebDriver _driver = new InitWebDriver();
+
+
+        public static InitWebDriver GetDriver()
+        {
+            return _driver;
+        }
+
+        static InitWebDriver InitWD()
+        {
+            switch (ConfigurationManager.AppSettings["browser"])
+            {
+
+            }
+            return _driver;
+        }
+    }
+}

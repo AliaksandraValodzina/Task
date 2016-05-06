a1276dc62518d18f7b10eac3d018cc69d4a2aec9
Aliaksandra Valodzina <shoora.volodina@gmail.com>
2016-05-06 13:50:13 +0300

    task

---
+++ Part8_Serialization/Part8_Serialization/Run.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Part8_Serialization
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Book
+    {
+        public string id;
+        public BookType Type;
+    }
+
+    public enum BookType
+    {
+        isbn,
+        author,
+        title,
+        genre,
+        publisher,
+        publish_date,
+        description
+    }
+
+    public XmlSerializer CreateOverrider()
+    {
+        XmlAttributeOverrides xOver = new XmlAttributeOverrides();
+        XmlAttributes xAttrs = new XmlAttributes();
+
+        // Add an XmlEnumAttribute for the FoodType.Low enumeration.
+    }
+}

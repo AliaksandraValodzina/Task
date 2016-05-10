b243427b8f2d938f9652b0f4737176b5a617597b
AlexandrVolodin <AlexandrVolodin@ALEXANDER>
2016-05-10 22:04:34 +0300

    task

--- Part8_Serialization/Part8_Serialization/Elements/Book.cs
+++ Part8_Serialization/Part8_Serialization/Elements/Book.cs
@@ -7,25 +7,47 @@ using System.Xml.Serialization;
 
 namespace Part8_Serialization.Elements
 {
+        public enum Genre
+        {
+            [XmlEnum("Computer")]
+            Computer,
+            [XmlEnum("Fantasy")]
+            Fantasy,
+            [XmlEnum("Romance")]
+            Romance,
+            [XmlEnum("Horror")]
+            Horror,
+            [XmlEnum("Science Fiction")]
+            ScienceFiction,
+
+        }
         [Serializable]
         public class Book
         {
             [XmlAttribute("id")]
             public string Id { get; set; }
+
             [XmlElement("isbn")]
             public string Isbn { get; set; } = "Not specified";
+
             [XmlElement("author")]
             public string Author { get; set; }
+
             [XmlElement("title")]
             public string Title { get; set; }
+
             [XmlElement("genre")]
             public Genre Genre { get; set; }
+
             [XmlElement("publisher")]
-            public string Publisher { get; set; }
+            public string Publish { get; set; }
+
             [XmlElement("publish_date")]
             public DateTime PublishDate { get; set; }
+
             [XmlElement("description")]
             public string Description { get; set; }
+
             [XmlElement("registration_date")]
             public DateTime RegistrationDate { get; set; }
 
@@ -38,7 +60,7 @@ namespace Part8_Serialization.Elements
                 Author = author;
                 Title = title;
                 Genre = genre;
-                Publisher = publisher;
+                Publish = publisher;
                 PublishDate = publishDate;
                 Description = description;
                 RegistrationDate = registrationDate;
@@ -46,7 +68,7 @@ namespace Part8_Serialization.Elements
             public override string ToString()
             {
                 return string.Format($" Author: {Author} -  Title: {Title}\n\tId: {Id}\n\tIsbn: {Isbn}"
-                    + $"\n\tGenre: {Genre}\n\tPublisher: {Publisher}\n\tPublish date: {PublishDate.ToString("MMMM dd yyyy")}"
+                    + $"\n\tGenre: {Genre}\n\tPublisher: {Publish}\n\tPublish date: {PublishDate.ToString("MMMM dd yyyy")}"
                     + $"\n\tDescription:{Description}\tRegistration date: {RegistrationDate.ToString("MMMM dd yyyy")}");
             }
 

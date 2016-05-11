0fa71e03c6f6887951e2d5f38e9ad683885bf90d
Aliaksandra Valodzina <shoora.volodina@gmail.com>
2016-05-11 15:28:54 +0300

    task

---
+++ Part9_WPF_Application/Part9_WPF_Application/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Part9_WPF_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var path = string.Empty;
+
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    path = dialog.SelectedPath;
+                }
+            }
+
+            if (path != string.Empty)
+            {
+                // Get path to .dll files 
+                var fileArray = Directory.GetFiles(path, "*.dll");
+
+                // Add path to textBox
+                tbPath.Text = path;
+
+                // Get fileName and add to ListBox
+                foreach (var file in fileArray)
+                {
+                    var fileName = Path.GetFileName(file);
+                    lbAssembles.Items.Add(fileName);
+                }
+            }
+
+
+        }
+    }
+}

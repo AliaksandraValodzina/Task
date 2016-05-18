476006dbfd7f807db9d3a883d7a5bd8d4e2c3cea
Aliaksandra Valodzina <shoora.volodina@gmail.com>
2016-05-18 11:39:51 +0300

    task

---
+++ Part10_Nuget_MSTest/MsTest/MsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using Part10_Nuget_MSTest;
+
+namespace MsTest
+{
+    [TestClass]
+    public class MsTest
+    {
+        private static BrowserController controller;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            controller = new BrowserController();
+            controller.Driver = new FirefoxDriver();
+            controller.Path = "http://www.google.com/";
+        }
+
+        [TestMethod]
+        public void TestMethodSetUp()
+        {
+            controller.Setup();
+        }
+
+        [TestMethod]
+        public void TestMethodTearDown()
+        {
+            controller.TearDown();
+        }
+    }
+
+    
+}
---
+++ Part10_Nuget_MSTest/Part10_Nuget_MSTest/BrowserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Part10_Nuget_MSTest
+{
+    public class BrowserController
+    {
+        public IWebDriver Driver { get; set; }
+        public string Path { get; set; }
+
+        public void Setup()
+        {
+            Driver.Navigate().GoToUrl(Path);
+        }
+
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
